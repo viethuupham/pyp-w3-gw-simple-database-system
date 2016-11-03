df70009eb7ede958e1cb339b337eb384c6c7f214
Martin Zugnoni <martin.zugnoni@gmail.com>
2016-11-03 18:25:24 -0300

    Provide public interface of Table and Database classes

--- simple_database/main.py
+++ simple_database/main.py
@@ -1,5 +1,57 @@
+import os
+from datetime import date
+
+from simple_database.exceptions import ValidationError
+from simple_database.config import BASE_DB_FILE_PATH
+
+
+class Table(object):
+
+    def __init__(self, db, name, columns=None):
+        pass
+
+    def insert(self, *args):
+        pass
+
+    def query(self, **kwargs):
+        pass
+
+    def all(self):
+        pass
+
+    def count(self):
+        pass
+
+    def describe(self):
+        pass
+
+
+class DataBase(object):
+    def __init__(self, name):
+        pass
+
+    @classmethod
+    def create(cls, name):
+        pass
+
+    def create_table(self, table_name, columns):
+        pass
+
+    def show_tables(self):
+        pass
+
+
 def create_database(db_name):
-    raise NotImplementedError()
+    """
+    Creates a new DataBase object and returns the connection object
+    to the brand new database.
+    """
+    DataBase.create(db_name)
+    return connect_database(db_name)
+
 
 def connect_database(db_name):
-    raise NotImplementedError()
+    """
+    Connectes to an existing database, and returns the connection object.
+    """
+    return DataBase(name=db_name)
